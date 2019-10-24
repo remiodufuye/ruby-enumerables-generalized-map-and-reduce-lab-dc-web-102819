6c8cdd98e115076a32b10ce5e5ef77bba17e6b3d
remi.odufuye <remi.odufuye@gmail.com>
2019-10-24 16:34:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+def map(block)
+  new = []
+  i = 0
+  while i < block.length
+    new.push(yield(block[i]))
+    i += 1
+  end
+  new
+end
+
+
+def reduce(block, special_condition =nil)
+  
+  if special_condition
+    accumulate_total = special_condition
+    i = 0
+  else
+    accumulate_total = block[0]
+    i = 1
+  end
+  while i < block.length
+    accumulate_total = yield(accumulate_total, block[i])
+    i += 1
+  end
+  accumulate_total
+end
+    
